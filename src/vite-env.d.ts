5a40ca2be5b163027a34ca4432d8e5a6575f6cfd
ZukaBri3k <kyrilldumerchat@gmail.com>
2024-09-23 20:42:46 +0200

    Base du projet

---
+++ src/vite-env.d.ts
@@ -0,0 +1,17 @@
+/// <reference types="vite/client" />
+
+// env.d.ts
+declare module "*.jpg" {
+    const value: string;
+    export default value;
+}
+
+declare module "*.png" {
+    const value: string;
+    export default value;
+}
+
+declare module "*.svg" {
+    const content: any;
+    export default content;
+}
