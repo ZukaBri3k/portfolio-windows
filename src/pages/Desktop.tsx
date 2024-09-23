0f0d8a7273fb2be71ad17eceb528ef3e86128b48
ZukaBri3k <kyrilldumerchat@gmail.com>
2024-09-23 20:42:59 +0200

    (create) création de la page bureau

---
+++ src/pages/Desktop.tsx
@@ -0,0 +1,7 @@
+import { Dock } from '@components/Dock';
+
+export default function Desktop() {
+  return <div className='bg-wallpaper bg-no-repeat bg-cover w-full h-full'>
+    <Dock />
+  </div>;
+}
