187806e2ad702a2f7f073ce19d85bfada051b5bd
ZukaBri3k <kyrilldumerchat@gmail.com>
2024-10-25 10:56:59 +0200

    (add) ajout de la navbar

--- src/pages/Desktop.tsx
+++ src/pages/Desktop.tsx
@@ -1,7 +1,25 @@
-import { Dock } from '@components/Dock';
+import { Taskbar } from "@components/Taskbar";
+import { useState } from "react";
+import { WindowsWindow } from "@windows/WindowsWindow.tsx";
+import { menuType } from "../types/menuType.ts";
 
 export default function Desktop() {
-  return <div className='bg-wallpaper bg-no-repeat bg-cover w-full h-full'>
-    <Dock />
-  </div>;
+  const [menuState, setMenuState] = useState<menuType>({
+    windowsMenu: {
+      open: false,
+      component: <WindowsWindow />,
+    },
+  });
+
+  return (
+    <div className="w-fit h-fit overflow-x-hidden overflow-y-hidden">
+      <div
+        className="bg-wallpaper bg-no-repeat bg-cover w-dvw h-dvh"
+        id="window"
+      >
+        <Taskbar setMenuState={setMenuState} menuState={menuState} />
+      </div>
+      {menuState.windowsMenu.open && menuState.windowsMenu.component}
+    </div>
+  );
 }
