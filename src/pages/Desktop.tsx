fb5e1ce757aa4cbda5e6a9d691f36fa79f555adf
kdumerchat <kyrill.dumerchat@kwh50.io>
2024-10-29 15:44:47 +0100

    (add) : ajout du menu FileExplorer.tsx

--- src/pages/Desktop.tsx
+++ src/pages/Desktop.tsx
@@ -1,28 +1,35 @@
-import { Taskbar } from "@components/Taskbar";
-import { useState } from "react";
-import { WindowsWindow } from "@windows/WindowsWindow.tsx";
-import { menuType } from "../types/menuType.ts";
-import { MenusContext } from "../context/menusContext.ts";
+import {Taskbar} from "@/components/Taskbar";
+import {useState} from "react";
+import {WindowsWindow} from "@/windows/WindowsWindow.tsx";
+import {menuType} from "../types/menuType.ts";
+import {MenusContext} from "../context/menusContext.ts";
+import {FileExplorer} from "@/windows/FileExplorer.tsx";
+
 
 export default function Desktop() {
   const [menuState, setMenuState] = useState<menuType>({
     windowsMenu: {
       open: false,
-      component: <WindowsWindow />,
+      component: <WindowsWindow/>,
     },
+    fileExplorerMenu: {
+      open: false,
+      component: <FileExplorer/>,
+    }
   });
 
   return (
-    <MenusContext.Provider value={{ menuState, setMenuState }}>
-      <div className="w-fit h-fit overflow-x-hidden overflow-y-hidden">
-        <div
-          className="bg-wallpaper bg-no-repeat bg-cover w-dvw h-dvh"
-          id="window"
-        >
-          <Taskbar setMenuState={setMenuState} menuState={menuState} />
+      <MenusContext.Provider value={{menuState, setMenuState}}>
+        <div className="w-fit h-fit overflow-x-hidden overflow-y-hidden">
+          <div
+              className="bg-wallpaper bg-no-repeat bg-cover w-dvw h-dvh"
+              id="window"
+          >
+            <Taskbar/>
+          </div>
+          {menuState.windowsMenu.open && menuState.windowsMenu.component}
+          {menuState.fileExplorerMenu.open && menuState.fileExplorerMenu.component}
         </div>
-        {menuState.windowsMenu.open && menuState.windowsMenu.component}
-      </div>
-    </MenusContext.Provider>
+      </MenusContext.Provider>
   );
 }
