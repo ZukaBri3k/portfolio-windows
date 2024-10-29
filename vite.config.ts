6c16b2f0f82a2e858c06a0061fbb8dceeb51a05a
kdumerchat <kyrill.dumerchat@kwh50.io>
2024-10-29 15:47:13 +0100

    (update) : changements des alias pour @/ en alias global

--- vite.config.ts
+++ vite.config.ts
@@ -7,10 +7,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@components": path.resolve(__dirname, "./src/components"),
-      "@pages": path.resolve(__dirname, "./src/pages"),
-      "@assets": path.resolve(__dirname, "./src/assets"),
-      "@windows": path.resolve(__dirname, "./src/windows"),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
