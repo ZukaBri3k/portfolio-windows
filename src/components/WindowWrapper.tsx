aa881e866704adcd1657e2c14e0fe44977e78f54
kdumerchat <kyrill.dumerchat@kwh50.io>
2024-10-29 15:47:41 +0100

    (add) : ajout de style ainsi que de la toolbar

--- src/components/WindowWrapper.tsx
+++ src/components/WindowWrapper.tsx
@@ -1,17 +1,56 @@
-import { ReactNode } from "react";
+import {HTMLAttributes, ReactNode, useContext} from "react";
 import Draggable from "react-draggable";
+import {Minus, Square, X} from "lucide-react";
+import {MenusContext} from "@/context/menusContext.ts";
 
-interface props {
+
+interface props extends HTMLAttributes<HTMLDivElement> {
   children: ReactNode;
+  title: string;
+  window: string;
 }
 
-export function WindowWrapper({ children }: props) {
+export function WindowWrapper({children, title, window}: props) {
+
+  const {setMenuState, menuState} = useContext(MenusContext);
+
+  function closeWindow() {
+    if (menuState) {
+      const windowState = menuState[window as keyof typeof menuState];
+      setMenuState((prev) => {
+        return {
+          ...prev,
+          [window]: {
+            ...windowState,
+            open: false
+          }
+        };
+      });
+    }
+  }
+
   return (
-    <Draggable bounds="parent">
-      <div className="w-[50dvw] h-[50dvh] bg-amber-300 z-20 flex flex-col">
-        <div className="h-5 w-full bg-slate-50"></div>
-        {children}
-      </div>
-    </Draggable>
+      <Draggable bounds="parent" handle="#handle">
+        <div
+            className="w-[60dvw] h-[70dvh] absolute bottom-[20dvh] right-[40dvh] translate-x-[60dvh] translate-y-[70dvh] bg-slate-900/90 z-20 flex flex-col rounded-xl border-[0.5px] border-slate-600 overflow-hidden">
+          <div className="h-10 w-full cursor-pointer flex items-center" id="tooltip">
+            <div id="handle" className="h-full w-full flex items-center justify-start pl-5">
+              <h2 className="text-slate-50">{title}</h2>
+            </div>
+            <div className="w-fit h-full flex items-center">
+              <button className="w-14 h-full hover:bg-slate-700 flex items-center justify-center" onClick={closeWindow}>
+                <Minus strokeWidth={1.5} color="#E2E8F0" size={20}/>
+              </button>
+              <button className="w-14 h-full hover:bg-slate-700 flex items-center justify-center">
+                <Square strokeWidth={1.5} color="#E2E8F0" size={15}/>
+              </button>
+              <button className="w-14 h-full hover:bg-red-700 flex items-center justify-center" onClick={closeWindow}>
+                <X strokeWidth={1.5} color="#E2E8F0" size={20}/>
+              </button>
+            </div>
+          </div>
+          {children}
+        </div>
+      </Draggable>
   );
 }
