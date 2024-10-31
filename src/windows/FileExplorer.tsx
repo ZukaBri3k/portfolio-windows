e3d9ac2a5d527c60fd4ffb4ebc88fe5dd74a40d3
kdumerchat <kyrill.dumerchat@kwh50.io>
2024-10-31 15:14:32 +0100

    (add) : ajout d'un début de style pour la fenêtre d'explorateur de fichier

--- src/windows/FileExplorer.tsx
+++ src/windows/FileExplorer.tsx
@@ -5,8 +5,11 @@ import {WindowWrapper} from "@/components/WindowWrapper.tsx";
 export function FileExplorer() {
   return createPortal(
       <WindowWrapper title="File explorer" window="fileExplorerMenu">
-        <div className="w-full h-full bg-stone-400">
-          <h1>Hello world !</h1>
+        <div className="w-full h-full flex flex-col">
+          <div className="h-14 bg-stone-800/75 w-full">
+
+          </div>
+          <div className="h-full w-full bg-stone-900 border-t-[0.5px] border-t-slate-600"></div>
         </div>
       </WindowWrapper>, document.getElementById("window") as HTMLElement);
 }
