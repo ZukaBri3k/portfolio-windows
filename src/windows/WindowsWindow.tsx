6fc63448fea3ee08c6630eeb02f2330c2d8f9448
ZukaBri3k <kyrilldumerchat@gmail.com>
2024-12-22 11:10:12 +0100

    correction du bug qui empechait de fermer la modale

--- src/windows/WindowsWindow.tsx
+++ src/windows/WindowsWindow.tsx
@@ -10,7 +10,7 @@ import {menuType} from "@/types/menuType.ts";
 export function WindowsWindow() {
   const modalRef = useRef<HTMLDivElement>(null);
   const {setMenuState} = useContext(MenusContext);
-
+  
   useEffect(() => {
     anime({
       targets: modalRef.current,
@@ -20,7 +20,7 @@ export function WindowsWindow() {
       easing: "linear",
     });
   }, []);
-
+  
   function close() {
     anime({
       targets: modalRef.current,
@@ -40,34 +40,34 @@ export function WindowsWindow() {
       },
     });
   }
-
+  
   return createPortal(
-      <div className="w-full h-full">
+    <div className="w-full h-full">
+      <div
+        ref={modalRef}
+        className="z-[100] absolute right-1/2 bottom-[-600px] translate-x-1/2 translate-y-1/2 lg:w-5/12 md:w-4/5 h-5/6 rounded-2xl bg-slate-800/95 backdrop-blur-xl flex justify-center items-center flex-col border-[0.5px] border-slate-600"
+      >
         <div
-            ref={modalRef}
-            className="z-[100] absolute right-1/2 bottom-[-600px] translate-x-1/2 translate-y-1/2 lg:w-5/12 md:w-4/5 h-5/6 rounded-2xl bg-slate-800/95 backdrop-blur-xl flex justify-center items-center flex-col border-[0.5px] border-slate-600"
-        >
-          <div
-              className="flex items-center w-11/12 h-fit gap-5 bg-slate-900 p-2 m-10 rounded-3xl border-[0.5px] border-slate-700">
-            <Search size={20} color="#E2E8F0" strokeWidth={1.5}/>
-            <input
-                className="w-full bg-slate-900 text-slate-300 outline-none"
-                type="text"
-                name="research"
-                placeholder="Recherchez des applications, des paramètres et des documents"
-            />
-          </div>
-          <div className="w-full h-full"></div>
-          <div className="w-full h-[100px] bg-slate-900/40 rounded-b-xl flex items-center pl-12 pr-12">
-            <div className="flex gap-2 items-center w-full">
-              <img src={User} alt="User profil picture" className="w-[50px]"/>
-              <p className="text-slate-300">Kyrill DUMERCHAT</p>
-            </div>
-            <Power size={24} strokeWidth={1.5} color="#E2E8F0"/>
-          </div>
+          className="flex items-center w-11/12 h-fit gap-5 bg-slate-900 p-2 m-10 rounded-3xl border-[0.5px] border-slate-700">
+          <Search size={20} color="#E2E8F0" strokeWidth={1.5}/>
+          <input
+            className="w-full bg-slate-900 text-slate-300 outline-none"
+            type="text"
+            name="research"
+            placeholder="Recherchez des applications, des paramètres et des documents"
+          />
         </div>
-        <div className="w-full h-full z-[99] absolute" onClick={close}/>
-      </div>,
-      document.getElementById("window") as HTMLElement
+        <div className="w-full h-full"></div>
+        <div className="w-full h-[100px] bg-slate-900/40 rounded-b-xl flex items-center pl-12 pr-12">
+          <div className="flex gap-2 items-center w-full">
+            <img src={User} alt="User profil picture" className="w-[50px]"/>
+            <p className="text-slate-300">Kyrill DUMERCHAT</p>
+          </div>
+          <Power size={24} strokeWidth={1.5} color="#E2E8F0"/>
+        </div>
+      </div>
+      <div className="w-full h-full z-[99] absolute top-0 left-0" onClick={close}/>
+    </div>,
+    document.getElementById("window") as HTMLElement
   );
 }
