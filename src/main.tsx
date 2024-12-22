de2f8559976dc339f8a7c46df8c0429fcec95094
ZukaBri3k <kyrilldumerchat@gmail.com>
2024-12-22 17:49:51 +0100

    Ajout d'un router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import {StrictMode} from "react";
+import {createRoot} from "react-dom/client";
 import "./globals.css";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Desktop from "@/pages/Desktop.tsx";
+import Login from "@/pages/Login.tsx";
 
 createRoot(document.getElementById("root")!).render(
-    <StrictMode>
-        <App />
-    </StrictMode>
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/desktop" element={<Desktop/>}/>
+        <Route path="/" element={<Login/>}/>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
 );
