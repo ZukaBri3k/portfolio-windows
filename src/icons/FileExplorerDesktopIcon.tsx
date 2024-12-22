66a21d337663c930e684cc4b0dddd564112d3c6f
ZukaBri3k <kyrilldumerchat@gmail.com>
2024-12-22 17:49:07 +0100

    RAS

--- src/icons/FileExplorerDesktopIcon.tsx
+++ src/icons/FileExplorerDesktopIcon.tsx
@@ -1,35 +1,36 @@
-import {DesktopIconWrapper} from "@/components/DesktopIconWrapper.tsx";
+import { DesktopIconWrapper } from "@/components/DesktopIconWrapper.tsx";
 import FileExplorerIcon from "@/assets/icons/file-explorer.png";
-import {useContext} from "react";
-import {MenusContext} from "@/context/menusContext.ts";
-
+import { useContext } from "react";
+import { MenusContext } from "@/context/menusContext.ts";
 
 export function FileExplorerDesktopIcon() {
-
-  const {setMenuState} = useContext(MenusContext);
+  const { setMenuState } = useContext(MenusContext);
 
   return (
-      <DesktopIconWrapper>
+    <DesktopIconWrapper>
+      <div className="absolute bottom-0 right-0 flex flex-col items-center justify-center hover:bg-slate-700/40 p-2 rounded">
         <div
-            className="absolute top-[100px] left-[100px] flex flex-col items-center justify-center hover:bg-slate-700/40 p-2 rounded">
-          <div
-              className="w-[64px] h-[64px]"
-              onDoubleClick={() => {
-                setMenuState((prev) => {
-                  return {
-                    ...prev,
-                    fileExplorerMenu: {
-                      ...prev.fileExplorerMenu,
-                      open: true
-                    }
-                  };
-                });
-              }}
-          >
-            <img className="pointer-events-none" src={FileExplorerIcon} alt="file explorer icon"/>
-          </div>
-          <p className="text-slate-50">File explorer</p>
+          className="w-[64px] h-[64px]"
+          onDoubleClick={() => {
+            setMenuState((prev) => {
+              return {
+                ...prev,
+                fileExplorerMenu: {
+                  ...prev.fileExplorerMenu,
+                  open: true,
+                },
+              };
+            });
+          }}
+        >
+          <img
+            className="pointer-events-none"
+            src={FileExplorerIcon}
+            alt="file explorer icon"
+          />
         </div>
-      </DesktopIconWrapper>
+        <p className="text-slate-50">File explorer</p>
+      </div>
+    </DesktopIconWrapper>
   );
 }
