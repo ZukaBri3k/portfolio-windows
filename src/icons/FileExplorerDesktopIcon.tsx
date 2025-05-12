be8bb16ff33f7f427191cfc9e1877404909aeb24
kdumerchat <kyrill.dumerchat@kwh50.io>
2025-05-12 14:26:36 +0200

    Refactor FileExplorerDesktopIcon component layout and add ref for context menu

--- src/icons/FileExplorerDesktopIcon.tsx
+++ src/icons/FileExplorerDesktopIcon.tsx
@@ -1,36 +1,42 @@
 import { DesktopIconWrapper } from "@/components/DesktopIconWrapper.tsx";
 import FileExplorerIcon from "@/assets/icons/file-explorer.png";
-import { useContext } from "react";
+import { useContext, useRef } from "react";
 import { MenusContext } from "@/context/menusContext.ts";
+import { FolderContextualMenu } from "@/components/FolderContextualMenu";
 
 export function FileExplorerDesktopIcon() {
   const { setMenuState } = useContext(MenusContext);
+  const ref = useRef<HTMLDivElement>(null);
 
   return (
-    <DesktopIconWrapper>
-      <div className="absolute bottom-0 right-0 flex flex-col items-center justify-center hover:bg-slate-700/40 p-2 rounded">
-        <div
-          className="w-[64px] h-[64px]"
-          onDoubleClick={() => {
-            setMenuState((prev) => {
-              return {
-                ...prev,
-                fileExplorerMenu: {
-                  ...prev.fileExplorerMenu,
-                  open: true,
-                },
-              };
-            });
-          }}
+    <>
+      <DesktopIconWrapper>
+        <div className="absolute top-2 left-2 flex flex-col items-center justify-center hover:bg-slate-700/40 p-2 rounded cursor-pointer" onDoubleClick={() => {
+          setMenuState((prev) => {
+            return {
+              ...prev,
+              fileExplorerMenu: {
+                ...prev.fileExplorerMenu,
+                open: true,
+              },
+            };
+          });
+        }}
+          ref={ref}
         >
-          <img
-            className="pointer-events-none"
-            src={FileExplorerIcon}
-            alt="file explorer icon"
-          />
+          <div
+            className="w-[64px] h-[64px]"
+          >
+            <img
+              className="pointer-events-none"
+              src={FileExplorerIcon}
+              alt="file explorer icon"
+            />
+          </div>
+          <p className="text-slate-50">File explorer</p>
         </div>
-        <p className="text-slate-50">File explorer</p>
-      </div>
-    </DesktopIconWrapper>
+      </DesktopIconWrapper>
+      <FolderContextualMenu handleRef={ref} />
+    </>
   );
 }
