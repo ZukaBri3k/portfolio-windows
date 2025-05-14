fadbe7caa0479420c5915b746d7726d8d9bd60b0
kdumerchat <kyrill.dumerchat@kwh50.io>
2025-05-14 10:18:12 +0200

    Refactor useCreateWindow hook to accept setWindows as a parameter, removing dependency on WindowsContext for improved flexibility.

--- src/hooks/useCreateWindow.tsx
+++ src/hooks/useCreateWindow.tsx
@@ -1,16 +1,14 @@
 
-import { WindowsContext } from "@/context/windowsContext";
 import { BraveBrowser } from "@/windows/BraveBrowser";
 import { FileExplorer } from "@/windows/FileExplorer";
-import { useContext } from "react";
 import FileExplorerIcon from "@/assets/icons/file-explorer.png";
 import BraveBrowserIcon from  "@/assets/icons/brave-browser.png";
+import { windowType } from "@/types/window.type";
 
-export function useCreateWindow() {
-  const { setWindows } = useContext(WindowsContext);
+export function useCreateWindow(setWindows: React.Dispatch<React.SetStateAction<windowType[]>>) {
 
   function createFileExplorerWindow() {
-    const windowId = Date.now().toString();
+    const windowId = Date.now().toString();   
     setWindows((prev) => [
       ...prev,
       { windowId, window: <FileExplorer key={windowId} windowId={windowId} />, icon: FileExplorerIcon },
