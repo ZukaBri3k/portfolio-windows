37f232aa93ccd8b72e4d65b7462693af75e6ffed
kdumerchat <kyrill.dumerchat@kwh50.io>
2025-05-15 14:54:07 +0200

    adding google web search engine

--- src/windows/BraveBrowser.tsx
+++ src/windows/BraveBrowser.tsx
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { createPortal } from "react-dom";
 import BraveBrowserIcon from "@/assets/icons/brave-browser.png";
 import { useHistory } from "@/hooks/useHistory";
@@ -10,32 +10,80 @@ interface props {
   windowId: string;
 }
 
-export function BraveBrowser({windowId} : props) {
+export function BraveBrowser({ windowId }: props) {
   const iframeRef = useRef<HTMLIFrameElement>(null);
-  const { canGoBack, canGoForward, currentURL, goBack, goForward, clearHistory } = useHistory();
+  const { canGoBack, canGoForward, currentURL, goBack, goForward, clearHistory, addToHistory } = useHistory();
+  const [inputURL, setInputURL] = useState<string>(currentURL);
+
+  useEffect(() => {
+    const script = document.createElement('script');
+    document.head.appendChild(script);
+    script.src = "https://cse.google.com/cse.js?cx=25d8ce44e9185495c";
+    script.async = true;
+  }, [])
 
   return createPortal(
     <WindowWrapper title="Brave browser" icon={BraveBrowserIcon} windowId={windowId}>
       <div className="w-full h-full flex flex-col items-center justify-center">
-        <div className="bg-stone-600 w-full p-2 flex items-center justify-start gap-3">
-          <button onClick={goBack} style={{ color: canGoBack() ? "oklch(98.5% 0.001 106.423)" : "oklch(55.3% 0.013 58.071)" }} disabled={!canGoBack}>
+        <div className="bg-stone-600 w-full p-2 flex items-center justify-start flex-shrink-0 gap-3">
+          <button
+            onClick={goBack}
+            style={{ color: canGoBack() ? "oklch(98.5% 0.001 106.423)" : "oklch(55.3% 0.013 58.071)" }}
+            disabled={!canGoBack()}
+          >
             <ChevronLeft />
           </button>
-          <button onClick={goForward} style={{ color: canGoForward() ? "oklch(98.5% 0.001 106.423)" : "oklch(55.3% 0.013 58.071)" }} disabled={!canGoForward}>
+          <button
+            onClick={goForward}
+            style={{ color: canGoForward() ? "oklch(98.5% 0.001 106.423)" : "oklch(55.3% 0.013 58.071)" }}
+            disabled={!canGoForward()}
+          >
             <ChevronRight />
           </button>
-          <button onClick={clearHistory} className="text-stone-50 px-2 py-1 rounded-md">
+          <button
+            onClick={clearHistory}
+            className="text-stone-50 px-2 py-1 rounded-md"
+          >
             <Home />
           </button>
-          <button onClick={() => {
-            if (iframeRef.current) {
-              iframeRef.current.src = currentURL;
-            }
-          }} className="text-stone-50 px-2 py-1 rounded-md">
+          <button
+            onClick={() => {
+              if (iframeRef.current) {
+                iframeRef.current.src = currentURL;
+              }
+            }}
+            className="text-stone-50 px-2 py-1 rounded-md"
+          >
             <RefreshCcw />
           </button>
+          <input
+            type="text"
+            value={inputURL}
+            className="flex-1 bg-stone-700 text-stone-50 rounded px-2 py-1"
+            onKeyDown={(e) => {
+              if (e.key === 'Enter') {
+                //navigateTo(inputURL);
+              }
+            }}
+            onChange={(e) => {
+              setInputURL(e.target.value);
+            }}
+          />
         </div>
-        <iframe src={currentURL} width="100%" height="100%" ref={iframeRef}></iframe>
+        <div className="w-full h-full flex-shrink overflow-y-auto">
+          <div className="gcse-search"></div>
+        </div>
+        {/* <iframe
+          src="https://cse.google.com/cse?cx=25d8ce44e9185495c"
+          className="w-full h-full flex-shrink"
+          ref={iframeRef}
+          onLoad={() => {
+            console.log("Page loaded");
+            // Au lieu d'essayer d'accéder à location.href, mettons à jour inputURL
+            setInputURL(currentURL);
+          }}
+          sandbox="allow-same-origin allow-scripts allow-popups allow-forms"
+        ></iframe> */}
       </div>
     </WindowWrapper>, document.getElementById("window") as HTMLElement);
 }
